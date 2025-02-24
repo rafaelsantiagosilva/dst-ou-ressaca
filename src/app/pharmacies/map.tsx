ba7e1065585d193ad8d314d5fb5ca65548ba7297
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-02-24 19:20:38 -0300

    fix: build bugs

--- src/app/pharmacies/map.tsx
+++ src/app/pharmacies/map.tsx
@@ -3,7 +3,7 @@
 import { IPharmacy } from '@/interfaces/IPharmacy';
 import { Icon, LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import {
 	MapContainer,
 	MapContainerProps,
@@ -14,7 +14,6 @@ import {
 
 interface MapProps extends MapContainerProps {
 	pharmacies?: IPharmacy[];
-	userPosition: LatLngExpression;
 }
 
 const userLocationIconMarker = new Icon({
@@ -31,43 +30,21 @@ const pharmacyIcon = new Icon({
 	popupAnchor: [-32, -64],
 });
 
-export function Map({
-	center,
-	zoom,
+export default function Map({
 	style = { height: '500px', width: '70%' },
-	userPosition,
 }: MapProps) {
+	const [lat, setLat] = useState(0);
+	const [lon, setLon] = useState(0);
 	const [pharmacies, setPharmacies] = useState<IPharmacy[]>([]);
 	const [error, setError] = useState<string | null>(null);
-	const [noPharmacies, setNoPharmacies] = useState<boolean>(false);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				const lat = position.coords.latitude;
-				const lon = position.coords.longitude;
-				searchPharmacies(lat, lon);
-			},
-			(err) => {
-				if (err.code === err.PERMISSION_DENIED) {
-					setError(
-						'Ei cara! Habilita a localização ai. Prometo que só vou compartilhar com profissionais.'
-					);
-				} else {
-					setError('Erro ao obter localização: ' + err.message);
-				}
-			},
-			{ enableHighAccuracy: true }
-		);
-	}, []);
-
-	const searchPharmacies = (lat: number, lon: number) => {
+	const searchPharmacies = useCallback((lat: number, lon: number) => {
 		const query = `
       [out:json];
       (
-        node["amenity"="pharmacy"](around:10000, ${lat}, ${lon});
-        way["amenity"="pharmacy"](around:10000, ${lat}, ${lon});
+        node["amenity"="pharmacy"](around:7000, ${lat}, ${lon});
+        way["amenity"="pharmacy"](around:7000, ${lat}, ${lon});
         relation["amenity"="pharmacy"](around:5000, ${lat}, ${lon});
       );
       out center;
@@ -78,85 +55,101 @@ export function Map({
 		)
 			.then((response) => response.json())
 			.then((data) => {
-				if (data.elements.length === 0) {
-					setNoPharmacies(true);
-				} else {
-					setPharmacies(data.elements);
-
-					if (pharmacies.length === 0)
-						setError(
-							'Aparentemente você mora em Xique-Xique (BA) e não há farmácias por perto.'
-						);
-
-					setNoPharmacies(false);
-					setIsLoading(false);
-				}
+				setPharmacies(data.elements);
+				setIsLoading(false);
 			})
 			.catch(() =>
 				setError(
 					'Erro ao buscar as farmácias, devem ser os hackers argentinos. Tente novamente mais tarde.'
 				)
 			);
-	};
+	}, []);
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const fixedLat = position.coords.latitude;
+				const fixedLon = position.coords.longitude;
+				// const fixedLat = -23.550164466;
+				// const fixedLon = -46.633664132;
+				setLat(fixedLat);
+				setLon(fixedLon);
+				searchPharmacies(fixedLat, fixedLon);
+			},
+			(err) => {
+				if (err.code === err.PERMISSION_DENIED) {
+					setError(
+						'Ei cara! Habilita a localização ai. Prometo que só vou compartilhar com profissionais.'
+					);
+				} else {
+					setError('Erro ao obter localização: ' + err.message);
+				}
+				setIsLoading(false);
+			},
+			{ enableHighAccuracy: true }
+		);
+	}, [searchPharmacies, lat, lon]);
 
 	return (
 		<div className="h-dvh">
 			{error && <p className="text-red-500 font-semibold text-xl mb-4">{error}</p>}
-			{noPharmacies && !error && (
-				<p className="text-yellow-500 font-semibold text-xl mb-4">
-					Nenhuma farmácia encontrada na sua área.
-				</p>
-			)}
-
 			{isLoading ? (
 				<div className="h-full font-bold text-xl text-center">Carregando...</div>
 			) : (
-				!error && (
-					<MapContainer
-						className="mx-auto border-4 border-slate-900 rounded-md"
-						center={center}
-						zoom={zoom}
-						style={style}
-					>
-						<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-						<Marker
-							position={userPosition}
-							alt="Sua localização"
-							title="Localização Atual"
-							riseOnHover
-							icon={userLocationIconMarker}
+				<>
+					{pharmacies.length === 0 && (
+						<p className="font-semibold text-xl mb-4">
+							Aparentemente você mora em Xique-Xique (BA) e não possui farmácias
+							próximas de você!{' '}
+						</p>
+					)}
+					{!error && (
+						<MapContainer
+							className="mx-auto border-4 border-slate-900 rounded-md"
+							center={[lat, lon]}
+							zoom={15}
+							style={style}
 						>
-							<Popup>Você está aqui!</Popup>
-						</Marker>
+							<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+							<Marker
+								position={[lat, lon] as LatLngExpression}
+								alt="Sua localização"
+								title="Localização Atual"
+								riseOnHover
+								icon={userLocationIconMarker}
+							>
+								<Popup>Você está aqui!</Popup>
+							</Marker>
 
-						{pharmacies.map((pharmacy) => {
-							const position = pharmacy.center
-								? [pharmacy.center.lat, pharmacy.center.lon]
-								: [pharmacy.lat, pharmacy.lon];
+							{pharmacies.map((pharmacy) => {
+								const position = pharmacy.center
+									? [pharmacy.center.lat, pharmacy.center.lon]
+									: [pharmacy.lat, pharmacy.lon];
 
-							return (
-								<Marker
-									position={position as LatLngExpression}
-									alt="Pino de farmácia"
-									title={pharmacy.tags.name}
-									riseOnHover
-									icon={pharmacyIcon}
-									key={pharmacy.id}
-								>
-									<Popup>
-										<strong>{pharmacy.tags.name}</strong>
-										<br />
-										{pharmacy.tags['addr:street'] && (
-											<small>
-												{pharmacy.tags['addr:street']} {pharmacy.tags['addr:housenumber']}
-											</small>
-										)}
-									</Popup>
-								</Marker>
-							);
-						})}
-					</MapContainer>
-				)
+								return (
+									<Marker
+										position={position as LatLngExpression}
+										alt="Pino de farmácia"
+										title={pharmacy.tags.name}
+										riseOnHover
+										icon={pharmacyIcon}
+										key={pharmacy.id}
+									>
+										<Popup>
+											<strong>{pharmacy.tags.name}</strong>
+											<br />
+											{pharmacy.tags['addr:street'] && (
+												<small>
+													{pharmacy.tags['addr:street']} {pharmacy.tags['addr:housenumber']}
+												</small>
+											)}
+										</Popup>
+									</Marker>
+								);
+							})}
+						</MapContainer>
+					)}
+				</>
 			)}
 		</div>
 	);
--- src/app/pharmacies/page.tsx
+++ src/app/pharmacies/page.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { Header } from '@/components/header';
-import { Map } from './map';
 import { Footer } from '@/components/footer';
+import dynamic from 'next/dynamic';
+
+const DynamicMap = dynamic(() => import('./map'), { ssr: false });
 
 export default function Pharmacies() {
 	return (
@@ -8,9 +12,8 @@ export default function Pharmacies() {
 			<Header />
 			<main className="p-4">
 				<h1 className="text-2xl font-bold text-center mb-4">Farmácias próximas</h1>
-				<Map
-					center={[-23.008011229111542, -46.84634048809668]}
-					userPosition={[-23.008011229111542, -46.84634048809668]}
+				<DynamicMap
+					center={[-23.550164466, -46.633664132]} // Praça da Sé
 					zoom={15}
 				/>
 			</main>
